d67e7507c47c93360c46260b004ee96632df3724
Dylan <dylangoodman@ymail.com>
2016-12-04 23:35:00 -0400

    Setting up node.

--- app/server/server.js
+++ app/server/server.js
@@ -1,3 +1,16 @@
 /**
  * Created by Dylan Goodman on 04-Dec-16.
  */
+
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(80);
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
