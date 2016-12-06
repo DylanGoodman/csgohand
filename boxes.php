8d07f81b41528c2e6822c50194df9bda04b2625d
Brett <freakernet@protonmail.com>
2016-12-05 22:28:29 -0600

    qqq

--- boxes.php
+++ boxes.php
@@ -8,10 +8,10 @@ require ('app/init.php');
 			<div class="main-page row" style="margin:0;">
 				<div class="col-md-8 grid_box1">
 					<div style="padding:0;background-color: #F1F1F1;" class="calender text-center">
-						<h4 class="title3 text-center">Your Bet</h4>
+						<h4 class="title3 text-center">Amount Wagered</h4>
                         <h4 style="background: #008aff" class="title3 text-center"><i class="fa fa-star"></i> 0</h4>
 
-						<div class="col-md-12 text-center">
+						<div style="margin-top: 10px;" class="col-md-12 text-center">
 							<h2>
 								<span class="fa-stack col-md-2">
 									<i style="color:#080505"  class="fa fa-square fa-stack-2x"></i>
