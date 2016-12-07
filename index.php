faefc261c9d3bd70c9437fef0c2f40ea1d5cee74
Brett <freakernet@protonmail.com>
2016-12-07 02:57:13 -0600

    adsf

--- index.php
+++ index.php
@@ -13,9 +13,9 @@ require ('app/init.php');
 						<?php if(!isset($_SESSION['steamid'])){ ?>
 							<h4 style="color:#fff;text-align:center;padding:0px 150px 0px 150px">The #1 Skin Platform is back and better than ever. We redesigned our website from the ground up
                         	in order to provide the greatest CS:GO gambling experience. With a <span style="color:#008aff">never before seen life-like Roulette experience</span>, Live Chat,
+							<span style="color:#008aff">Leveling 0-30 with rewards</span>, Refferals, and a hell of a lot more on the way!  </h4>
 						<?php } ?>
-						<span style="color:#008aff">Leveling 0-30 with rewards</span>, Refferals, and a hell of a lot more on the way!  </h4>
-                        <br>
+						<br>
 						<div style="margin-top:0;" class="widget_1 row elements">
 							<div class="col-sm-4 widget_1_box">
 								<div style="width:100%;padding:7px;" class="widget_1_box wid-social vimeo">
