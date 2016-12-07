61073bc5cd1559d638b1a56e6ded17c2660ca391
Brett <freakernet@protonmail.com>
2016-12-06 23:58:54 -0600

    green

--- roulette.php
+++ roulette.php
@@ -105,7 +105,7 @@ require ('app/init.php');
 
 						<div style="margin-top:0" class="row">
 							<button style="border-radius:0;" type="clear" class="btn col-md-4 btn-default">Black</button>
-							<button style="border-radius:0;color:#55AA55" type="clear" class="btn col-md-4  btn-default">Green</button>
+							<button style="border-radius:0;color:#55AA55" type="clear" class="btn col-md-4  btn-r-green">Green</button>
 							<button style="border-radius:0;color:#A43741" type="clear" class="btn col-md-4  btn-default">Red</button>
 						</div>
 
