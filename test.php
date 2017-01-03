fe4046e74996186b63f1c18e5a970a8e59fe586e
Dylan <dylangoodman@ymail.com>
2017-01-02 21:35:48 -0400

    Setting Up Prices

--- test.php
+++ test.php
@@ -12,9 +12,9 @@ $url = 'https://api.opskins.com/IPricing/GetAllLowestListPrices/v1?appid=730';
 $json = file_get_contents($url);
 
 $data = json_decode($json);
-$data = get_object_vars($data->response);
 
-foreach ($data as $item){
+
+foreach ($data->response as $item){
     print_r($item);
     $name = array_keys($item);
     $price = $item['price'];
