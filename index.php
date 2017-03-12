f0b30410573c704c59b4c24d1dbdd9d1b02a3d15
Dylan <dylangoodman@ymail.com>
2017-03-12 03:48:10 -0300

    Reorg

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+error_reporting(E_ALL);
 $init = $_SERVER['DOCUMENT_ROOT'];
-require $init.'/app/init.php';
+require $init.'/insta/app/init.php';
 if(isset($_SESSION['username'])) {
   header('Location: /insta/home');
   exit();
